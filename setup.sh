1bf3784a3c6611dbf6151a6a85641c0c59975756
ShadowVMX <92258683+ShadowVMX@users.noreply.github.com>
2022-12-06 19:33:07 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,55 @@
+!/bin/bash
+#
+#
+#Instalación de la herramienta fireshark.
+#
+#
+#########################################################################
+#                                                                       #
+#Desarrollado por ShadowVMX (aka) Juanma Coello para IFPS Puenteuropa.  #
+#                                                                       #
+#                         06/12/2022                                    #
+#                                                                       #
+#########################################################################
+#
+#
+#
+
+echo " "
+echo "$(tput setaf 6)[$(tput setaf 1)*$(tput setaf 6)]$(tput setaf 3)Proceso de Instalación en marcha ... "
+echo " "
+
+echo " "
+echo "$(tput setaf 6)[$(tput setaf 2)*$(tput setaf 6)]$(tput setaf 3)Updating ... "
+echo " "
+
+apt update -y &> /dev/null 
+
+echo " "
+echo "$(tput setaf 6)[$(tput setaf 2)*$(tput setaf 6)]$(tput setaf 3)Upgrading ... "
+echo " "
+
+apt upgrade -y &> /dev/null
+
+echo " "
+echo "$(tput setaf 6)[$(tput setaf 2)*$(tput setaf 6)]$(tput setaf 3)tcpdump ... "
+echo " "
+
+apt install tcpdump -y &> /dev/null
+
+echo " "
+echo "$(tput setaf 6)[$(tput setaf 2)*$(tput setaf 6)]$(tput setaf 3)figlet ... "
+echo " "
+
+apt install figlet -y &>/dev/null
+
+echo " "
+echo "$(tput setaf 6)[$(tput setaf 2)*$(tput setaf 6)]$(tput setaf 3)fireshark ... "
+echo " "
+
+chmod +x fireshark &> /dev/null
+cp fireshark /usr/bin &> /dev/null
+
+echo " "
+echo "$(tput setaf 6)[$(tput setaf 1)*$(tput setaf 6)]$(tput setaf 3)Proceso Finalizado ... "
+echo " "
